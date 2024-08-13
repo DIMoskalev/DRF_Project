9894c388b7ea3e5d068e3582b79b925002a28221
Dmitry Moskalev <moskalev199618120@gmail.com>
2024-08-13 21:24:57 +0300

    add fields milage for car and moto (with description)

--- vehicle/urls.py
+++ vehicle/urls.py
@@ -4,7 +4,7 @@ from vehicle.apps import VehicleConfig
 from rest_framework.routers import DefaultRouter
 
 from vehicle.views import CarViewSet, MotoCreateAPIView, MotoListAPIView, MotoRetrieveAPIView, MotoUpdateAPIView, \
-    MotoDestroyAPIView, MilageCreateAPIView
+    MotoDestroyAPIView, MilageCreateAPIView, MotoMilageListAPIView
 
 app_name = VehicleConfig.name
 
@@ -17,6 +17,8 @@ urlpatterns = [
     path('moto/<int:pk>/', MotoRetrieveAPIView.as_view(), name='moto_get'),
     path('moto/update/<int:pk>/', MotoUpdateAPIView.as_view(), name='moto_update'),
     path('moto/delete/<int:pk>/', MotoDestroyAPIView.as_view(), name='moto_delete'),
+
     # millage
     path('milage/create/', MilageCreateAPIView.as_view(), name='milage_create'),
+    path('moto/milage/', MotoMilageListAPIView.as_view(), name='moto_milage'),
 ] + router.urls
